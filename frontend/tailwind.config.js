3e954ae1877fd2f64759856fbdfc96cf5ce89e81
Frank Sandqvist <kankki@gmail.com>
2022-11-04 21:03:47 +0200

    ...

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: [
+        "Syne",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "helvetica",
+        "arial",
+      ],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
