2394e1bfe7d00a02dd8fc1cdb6bb93791400023d
Vladimir Kilyazov <west957@mail.ru>
2022-11-05 19:06:31 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-#import config
 from PIL import Image
 import os
 from flask_cors import CORS, cross_origin
@@ -14,6 +13,12 @@ from flask import Flask, request, render_template, send_file
 from clipmodel import EmbedderOpenCLIP
 from indexer import Indexer
 
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
 def readb64(uri):
    encoded_data = str(uri).split(',')[1]
    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
@@ -21,7 +26,6 @@ def readb64(uri):
    return img
 
 df = pd.read_csv('file2.csv')
-#clip_embedder = EmbedderOpenCLIP()
 clip_embedder = EmbedderOpenCLIP(device = 'cpu', clip_model_name = 'ViT-B-16-plus-240', pretrained = 'laion400m_e32')
 
 app = Flask(__name__)
@@ -36,20 +40,19 @@ def helloworld():
 @app.route('/search_by_image', methods=['POST'])
 @cross_origin()
 def search_by_image():
-    image_undecoded = request.get_data()
-    img = readb64(image_undecoded)
-    #print(img)
-    #print(image_undecoded)
-    #image_undecoded = image_undecoded.split(',')[1]
-    #print(img)
-    print(img.shape)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    content = request.json
+    if content['type'] == 'base64':
+        image_undecoded = content['image']
+        img = readb64(image_undecoded)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif content['type'] == 'url':
+        img = url_to_image(content['image'])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(img)
     emb = clip_embedder.encode_imgs([img])
     dists, indexes = indexer.find(emb, topn = 5)
-    ##reverse image bgr rgb
     loc = df.iloc[indexes]
-    return {'dists': str(list(dists)), 'names' : list(loc.Name.values), 'urls': str(list(loc.Url.values)),  'db_size': str(len(df.index))}
+    return {'dists': str(list(dists)), 'names' : list(loc.Name.values), 'urls': str(list(loc.Url.values)), 'db_size': str(len(df.index))}
 
 
 @app.route('/search_by_prompt', methods=['POST'])
@@ -62,10 +65,8 @@ def search_by_prompt():
     dists, indexes = indexer.find(emb,topn = amount)
     loc = df.iloc[indexes]
     return {'dists': str(list(dists)), 'names' : list(loc.Name.values), 'urls': str(list(loc.Url.values)), 'db_size': str(len(df.index))}
-    #return {'dists':str(dists), 'indexes':str(indexes)}
 
 
 if __name__ == '__main__':
     print("Starting server on port 8000")
     app.run(host="0.0.0.0", port=8000)
-
